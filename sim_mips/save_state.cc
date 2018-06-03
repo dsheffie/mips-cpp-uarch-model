f15aa15af4291492c55445685cac0ac74213fd1c
David Sheffield <sheffield.david@gmail.com>
2018-06-03 08:55:59 -0700

    pulled in save_state

---
+++ sim_mips/save_state.cc
@@ -0,0 +1,54 @@
+#include <boost/dynamic_bitset.hpp>
+#include <iostream>
+#include <cstdint>
+#include <cassert>
+#include <fcntl.h>
+
+#include "sparse_mem.hh"
+#include "save_state.hh"
+
+struct page {
+  uint32_t va;
+  uint8_t data[4096];
+} __attribute__((packed));
+
+struct header {
+  uint64_t magic;
+  uint32_t pc;
+  int32_t gpr[32];
+  int32_t lo;
+  int32_t hi;
+  uint32_t cpr0[32];
+  uint32_t cpr1[32];
+  uint32_t fcr1[5];
+  uint64_t icnt;
+  uint32_t num_nz_pages;
+} __attribute__((packed));
+
+
+void loadState(state_t &s, const std::string &filename) {
+  int fd = ::open(filename.c_str(), O_RDONLY, 0600);
+  assert(fd != -1);
+  header h;
+  size_t sz = read(fd, &h, sizeof(h));
+  assert(sz == sizeof(h));
+  
+  s.pc = h.pc;
+  memcpy(&s.gpr,&h.gpr,sizeof(s.gpr));
+  s.lo = h.lo;
+  s.hi = h.hi;
+  memcpy(&s.cpr0,&h.cpr0,sizeof(s.cpr0));
+  memcpy(&s.cpr1,&h.cpr1,sizeof(s.cpr1));
+  memcpy(&s.fcr1,&h.fcr1,sizeof(s.fcr1));
+  s.icnt = h.icnt;
+  
+  for(uint32_t i = 0; i < h.num_nz_pages; i++) {
+    page p;
+    sz = read(fd, &p, sizeof(p));
+    assert(sz == sizeof(p));
+    for(uint32_t j = 0; j < 4096; j++) {
+      s.mem.set(p.va+j, p.data[j]);
+    }
+  }
+  close(fd);
+}
