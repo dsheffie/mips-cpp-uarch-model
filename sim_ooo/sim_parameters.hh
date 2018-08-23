ac169461b087a5e81db91217fb6d8d61b42b9b47
David Sheffield <sheffield.david@gmail.com>
2018-08-22 21:03:47 -0700

    using x macros for parameter lists

--- sim_ooo/sim_parameters.hh
+++ sim_ooo/sim_parameters.hh
@@ -1,35 +1,40 @@
 #ifndef sim_parameters_hh
 #define sim_parameters_hh
 
+#define SIM_PARAM_LIST(X)			\
+  X(rob_size)					\
+  X(fetchq_size)				\
+  X(decodeq_size)				\
+  X(fetch_bw)					\
+  X(decode_bw)					\
+  X(alloc_bw)					\
+  X(retire_bw)					\
+  X(num_gpr_prf)				\
+  X(num_cpr0_prf)				\
+  X(num_cpr1_prf)				\
+  X(num_fcr1_prf)				\
+  X(num_fpu_ports)				\
+  X(num_alu_ports)				\
+  X(num_load_ports)				\
+  X(num_store_ports)				\
+  X(num_alu_sched_entries)			\
+  X(num_fpu_sched_entries)			\
+  X(num_jmp_sched_entries)			\
+  X(num_load_sched_entries)			\
+  X(num_store_sched_entries)			\
+  X(num_system_sched_entries)			\
+  X(load_tbl_size)				\
+  X(store_tbl_size)				\
+  X(taken_branches_per_cycle)
+
 namespace sim_param {
-  extern int rob_size;
-  extern int fetchq_size;
-  extern int decodeq_size;
-  
-  extern int fetch_bw;
-  extern int decode_bw;
-  extern int alloc_bw;
-  extern int retire_bw;
-  
-  extern int num_gpr_prf;
-  extern int num_cpr0_prf;
-  extern int num_cpr1_prf;
-  extern int num_fcr1_prf;
-  extern int num_fpu_ports;
-  extern int num_alu_ports;
-  
-  extern int num_load_ports;
-  extern int num_store_ports;
-  
-  extern int num_alu_sched_entries; 
-  extern int num_fpu_sched_entries;
-  extern int num_jmp_sched_entries; 
-  extern int num_load_sched_entries; 
-  extern int num_store_sched_entries;
-  extern int num_system_sched_entries;
-  
-  extern int load_tbl_size;
-  extern int store_tbl_size;
-  extern int taken_branches_per_cycle;
+#define ENTRY(X) extern int X;
+  SIM_PARAM_LIST(ENTRY);
+#undef ENTRY
 }
+
+#ifndef SAVE_SIM_PARAM_LIST
+#undef SIM_PARAM_LIST
+#endif
+
 #endif
