5e7d45d3a929b40a4f90a3ef978230e9071926ca
David Sheffield <sheffield.david@gmail.com>
2018-08-26 14:57:08 -0700

    print average number of ready insns in schedulers

--- sim_ooo/mips_op.hh
+++ sim_ooo/mips_op.hh
@@ -316,6 +316,8 @@ struct sim_state {
   uint64_t mispredicted_jalrs = 0;
   uint64_t nukes = 0, branch_nukes = 0, load_nukes = 0;
 
+  uint64_t total_ready_insns = 0;
+  
   sim_stack_template<uint32_t> return_stack;
 
   state_t *ref_state = nullptr;
