928b527c69f73c1cefef6781e2090b3d2796118b
David Sheffield <sheffield.david@gmail.com>
2018-09-21 17:34:13 -0700

    regex fixed

--- sim_ooo/scripts/plot.py
+++ sim_ooo/scripts/plot.py
@@ -8,7 +8,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-pre = re.compile('(\d+) cycles, (\d+) insns retired, avg ipc (\d+(.\d+)?), window ipc (\d+(.\d+)?)(, dcu hit rate \d+.\d+, window dcu hit rate \d+.\d+)?\n')
+pre = re.compile('c (\d+), i (\d+), a ipc (\d+(.\d+)?), w ipc (\d+(.\d+)?), a br (\d+(.\d+)?), w br (\d+(.\d+)?)(, dcu hit rate \d+.\d+, window dcu hit rate \d+.\d+)?\n')
 
 z = list()
 max_cycle = 0
