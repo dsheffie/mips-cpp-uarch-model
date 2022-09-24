e3a4b20f66b12c5ec23ac4efcba9cfa6f28b190e
David Sheffield <sheffield.david@gmail.com>
2022-09-23 20:45:11 -0700

    reduce number of ports

--- sim_ooo/sim_parameters.hh
+++ sim_ooo/sim_parameters.hh
@@ -15,8 +15,8 @@
   SIM_PARAM(num_cpr0_prf,64,32,true)		\
   SIM_PARAM(num_cpr1_prf,64,32,true)		\
   SIM_PARAM(num_fcr1_prf,16,8,true)			\
-  SIM_PARAM(num_fpu_ports,2,1,false)				\
-  SIM_PARAM(num_alu_ports,2,1,false)					\
+  SIM_PARAM(num_fpu_ports,1,1,false)				\
+  SIM_PARAM(num_alu_ports,1,1,false)					\
   SIM_PARAM(num_load_ports,1,1,false)					\
   SIM_PARAM(num_store_ports,1,1,false)					\
   SIM_PARAM(num_fpu_sched_per_cycle,1,1,false)				\
